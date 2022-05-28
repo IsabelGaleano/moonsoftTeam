f6d1b06a95524d6c6000e42409c8e4fbd7762b4d
Isabel <isagaleano9@gmail.com>
2022-05-27 23:29:07 -0600

    Changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var folder = path.join(__dirname, 'public'); //Definiendo que hay una carpeta pu
 app.use(express.static(folder)) //Use la carpeta public
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 5000, function () {
     console.log('Example app listening on port 3000!');
 });
 
