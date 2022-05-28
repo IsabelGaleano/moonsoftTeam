cc87a9368873f343bff0485c5baed61fafcf9f39
Isabel <isagaleano9@gmail.com>
2022-05-27 23:09:07 -0600

    Changes

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var app = express(); //Inicializa express
  
 var folder = path.join(__dirname, 'public'); //Definiendo que hay una carpeta public
 app.use(express.static(folder)) //Use la carpeta public
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+
+
+// app.listen(3000, function () {
+//     console.log('Example app listening on port 3000!');
+// });
+
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
+
